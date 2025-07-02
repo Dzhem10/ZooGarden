d7b4a50c4d644cbfdc928226f44a30caba0d795f
Salidzhan <salicanosman2000@gmail.com>
2025-07-03 00:01:33 +0300

    Add files via upload
    Добавен е файл Animal.cs, съдържащ класа Animal с основни свойства, конструктор и метод за форматиране на данни за запис във файл.

---
+++ Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ZooGarden
+{
+    public class Animal
+    {
+
+        // Свойства за всяко животно
+        public int AnimalID { get; set; }
+        public string Species { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Habitat { get; set; }
+        public bool Availability { get; set; }
+
+        // Конструктор за създаване на животно
+        public Animal(int id, string species, string name, int age, string habitat, bool availability)
+        {
+            AnimalID = id;
+            Species = species;
+            Name = name;
+            Age = age;
+            Habitat = habitat;
+            Availability = availability;
+        }
+
+        // Метод за преобразуване на животното към формат за запис във файл
+        public string ToFileFormat()
+        {
+            return $"{AnimalID};{Species};{Name};{Age};{Habitat};{Availability}";
+        }
+    }
+}
